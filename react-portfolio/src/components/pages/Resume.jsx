ee37ad030e1dbf7861738fed35f34205a4319156
Benita <benita.isaac@gmail.com>
2024-04-09 23:57:20 -0700

    added text to resume

--- react-portfolio/src/components/pages/Resume.jsx
+++ react-portfolio/src/components/pages/Resume.jsx
@@ -1,9 +1,29 @@
-function Resume(){
-    return(
-        <>
-        <h1> This is the Resume Page </h1>
-        </>
-    )
+function Resume() {
+  return (
+    <div className="resume-container">
+      <h2>Resume</h2>
+      <div className="resume-section">
+        <h3>Front-end Proficiencies</h3>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>Javascript</p>
+        <p>JQuery</p>
+        <p>Responsive Design</p>
+        <p>React</p>
+        <p>Bootstrap</p>
+      </div>
+      <div className="resume-section">
+        <h3>Back-end Proficiencies</h3>
+        <p>APIs</p>
+        <p>Node</p>
+        <p>Express</p>
+        <p>MySQL, Sequelize</p>
+        <p>MongoDB, Mongoose</p>
+        <p>REST</p>
+        <p>GraphQL</p>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
