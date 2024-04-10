792db228d2f28d91fa5354f422cedc5fc75b09ad
Benita <benita.isaac@gmail.com>
2024-04-09 17:51:53 -0700

    added router and paths

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "resume",
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
