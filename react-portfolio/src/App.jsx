24fcea9d46e3229e08cfa713a3b047d39c3b4c6e
Benita <benita.isaac@gmail.com>
2024-04-09 17:50:54 -0700

    cleaning up starter code

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-      <div>
+    <div>
       <Header />
-      </div>
+    </div>
   );
 }
 
-export default App
+export default App;
