293ab99df3c98c518a824456611f520260f6570a
Benita <benita.isaac@gmail.com>
2024-04-09 23:04:31 -0700

    added code for all list items in nav bar

--- react-portfolio/src/components/Navigation.jsx
+++ react-portfolio/src/components/Navigation.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Navigation() {
+function Navigation({currentPage}) {
   return (
-    <nav className="navbar">
-      <ul className="nav-menu">
-        <li className="nav-item"><a href="#about">About me</a></li>
-        <li className="nav-item"><a href="#portfolio">Portfolio</a></li>
-        <li className="nav-item"><a href="#contact">Contact</a></li>
-        <li className="nav-item"><a href="#resume">Resume</a></li>
+    <nav>
+      <ul className="flex-row">
+        <li className='m1'>
+          <Link to="/"> About </Link>
+        </li>
+        <li className='m1'>
+          <Link to="/portfolio"> Portfolio </Link>
+        </li>
+        <li className='m1'>
+          <Link to="/contact"> Contact </Link>
+        </li>
+        <li className='m1'>
+          <Link to="/resume"> Resume </Link>
+        </li>
       </ul>
     </nav>
+   
   );
 }
 
 export default Navigation;
+
+ // <nav className="navbar">
+    //   <ul className="nav-menu">
+    //     <li className="nav-item"><a href="#about">About me</a></li>
+    //     <li className="nav-item"><a href="#portfolio">Portfolio</a></li>
+    //     <li className="nav-item"><a href="#contact">Contact</a></li>
+    //     <li className="nav-item"><a href="#resume">Resume</a></li>
+    //   </ul>
+    // </nav>
