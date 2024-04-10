8b237ce792eb67e71f0bb31a970ef864ff167740
Benita <benita.isaac@gmail.com>
2024-04-10 00:20:13 -0700

    added text to about page

--- react-portfolio/src/components/pages/About.jsx
+++ react-portfolio/src/components/pages/About.jsx
@@ -1,9 +1,16 @@
-function About(){
-    return(
-        <>
-        <h1> This is the About Page </h1>
-        </>
-    )
+// import benitaImage from "../../assets/about/benita.JPG"
+
+function About() {
+  return (
+    <>
+    {/* <img src={benitaImage} alt="an image of Benita"></img> */}
+      <h1> About Benita </h1>
+      <p> Hi there! My name is Benita, and I'm embarking on an exciting journey into the world of web development. Coming from a background in non-profit work, I've honed strong leadership and soft skills that have prepared me well for this transition. Throughout my career, I've thrived in collaborative team environments and navigated professional settings with ease. </p>
+      <p> My academic background is rooted in mathematics, with a minor in STEM education. This foundation has equipped me with a solid understanding of coding principles and the ability to grasp complex concepts quickly. What sets me apart is my innate curiosity and drive to delve deeper into the 'why' behind code. My mathematical mindset compels me to explore the fundamental principles that underpin the functionality of algorithms and programs. </p>
+      <p> Recently, I completed a rigorous full stack web development bootcamp, where I acquired practical skills in frontend and backend development, along with proficiency in various technologies and frameworks. I'm fueled by my passion for problem-solving and creativity, and I'm eager to leverage my newfound skills to make meaningful contributions in the field of web development. </p>
+      <p> I'm excited about the opportunities that lie ahead and look forward to connecting with like-minded individuals and organizations as I embark on this exciting career switch. </p>
+    </>
+  );
 }
 
 export default About;
