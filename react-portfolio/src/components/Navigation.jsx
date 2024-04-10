9d5917447040572b398b56f8abdef7c29e59bc95
Benita <benita.isaac@gmail.com>
2024-04-09 23:57:05 -0700

    added code to make navigation work

--- react-portfolio/src/components/Navigation.jsx
+++ react-portfolio/src/components/Navigation.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-function Navigation({currentPage}) {
+function Navigation() {
+  const currentPage = useLocation().pathname;
+
   return (
     <nav>
       <ul className="flex-row">
         <li className='m1'>
-          <Link to="/"> About </Link>
+          <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}> About </Link>
         </li>
         <li className='m1'>
-          <Link to="/portfolio"> Portfolio </Link>
+          <Link to="/portfolio" className={currentPage === '/portfolio' ? 'nav-link active' : 'nav-link'}> Portfolio </Link>
         </li>
         <li className='m1'>
-          <Link to="/contact"> Contact </Link>
+          <Link to="/contact" className={currentPage === '/contact' ? 'nav-link active' : 'nav-link'}> Contact </Link>
         </li>
         <li className='m1'>
-          <Link to="/resume"> Resume </Link>
+          <Link to="/resume" className={currentPage === '/resume' ? 'nav-link active' : 'nav-link'}> Resume </Link>
         </li>
       </ul>
     </nav>
