f29987e7b3a7a737b14ff514b1dd53be7ecee5c1
Benita <benita.isaac@gmail.com>
2024-04-10 01:32:51 -0700

    added eventlisteners to navbar code for highlighting

--- react-portfolio/src/components/Navigation.jsx
+++ react-portfolio/src/components/Navigation.jsx
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function Navigation() {
+  const [currentSection, setCurrentSection] = useState("about"); // Initialize current section state
   const currentPage = useLocation().pathname;
+
+  // Function to handle click event on navigation items
+  const handleNavigationClick = (section) => {
+    setCurrentSection(section);
+  };
 
   return (
     <nav>
       <ul className="flex-row">
         <li className='m1'>
-          <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}> About </Link>
+          <Link
+            to="/"
+            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+            onClick={() => handleNavigationClick('about')}
+          >
+            About
+          </Link>
         </li>
         <li className='m1'>
-          <Link to="/portfolio" className={currentPage === '/portfolio' ? 'nav-link active' : 'nav-link'}> Portfolio </Link>
+          <Link
+            to="/portfolio"
+            className={currentPage === '/portfolio' ? 'nav-link active' : 'nav-link'}
+            onClick={() => handleNavigationClick('portfolio')}
+          >
+            Portfolio
+          </Link>
         </li>
         <li className='m1'>
-          <Link to="/contact" className={currentPage === '/contact' ? 'nav-link active' : 'nav-link'}> Contact </Link>
+          <Link
+            to="/contact"
+            className={currentPage === '/contact' ? 'nav-link active' : 'nav-link'}
+            onClick={() => handleNavigationClick('contact')}
+          >
+            Contact
+          </Link>
         </li>
         <li className='m1'>
-          <Link to="/resume" className={currentPage === '/resume' ? 'nav-link active' : 'nav-link'}> Resume </Link>
+          <Link
+            to="/resume"
+            className={currentPage === '/resume' ? 'nav-link active' : 'nav-link'}
+            onClick={() => handleNavigationClick('resume')}
+          >
+            Resume
+          </Link>
         </li>
       </ul>
     </nav>
-   
   );
 }
 
