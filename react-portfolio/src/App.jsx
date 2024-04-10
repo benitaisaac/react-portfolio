7db3db0b8ed621c308bd89d8d61c3007f52644c7
Benita <benita.isaac@gmail.com>
2024-04-09 23:55:47 -0700

    added Outlet in order to render navbar

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import { useLocation } from "react-router-dom";
+import {Outlet} from 'react-router-dom';
+
 
 function App() {
   // const [count, setCount] = useState(0)
+  const currentPage = useLocation().pathname;
+
 
   return (
     <div>
       <Header />
+      <Outlet />
+
     </div>
   );
 }
