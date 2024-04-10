cc148f82576b8483b629c5fc96f7765af0b6a90b
Benita <benita.isaac@gmail.com>
2024-04-09 23:58:38 -0700

    cleaned up code

--- react-portfolio/src/components/Navigation.jsx
+++ react-portfolio/src/components/Navigation.jsx
@@ -26,12 +26,3 @@ function Navigation() {
 }
 
 export default Navigation;
-
- // <nav className="navbar">
-    //   <ul className="nav-menu">
-    //     <li className="nav-item"><a href="#about">About me</a></li>
-    //     <li className="nav-item"><a href="#portfolio">Portfolio</a></li>
-    //     <li className="nav-item"><a href="#contact">Contact</a></li>
-    //     <li className="nav-item"><a href="#resume">Resume</a></li>
-    //   </ul>
-    // </nav>
