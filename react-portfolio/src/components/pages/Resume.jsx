b78731886cd0a9ce2fa5d0d6de04a80d7ea8cd6a
Benita <benita.isaac@gmail.com>
2024-04-10 22:53:59 -0700

    added downloadable resume

--- react-portfolio/src/components/pages/Resume.jsx
+++ react-portfolio/src/components/pages/Resume.jsx
@@ -1,4 +1,7 @@
 function Resume() {
+  const resumeUrl =
+    "https://docs.google.com/document/d/1yTp1EKI4YM4LJKeuycErITn2G4Vg0y5ry4cCg7ymgmM/edit?usp=sharing";
+
   return (
     <div className="resume-container">
       <h2>Resume</h2>
@@ -22,8 +25,12 @@ function Resume() {
         <p>REST</p>
         <p>GraphQL</p>
       </div>
+      <a href={resumeUrl} download>
+        <button className="button-10" role="button">Download Resume</button>
+      </a>
     </div>
   );
 }
 
 export default Resume;
+
