5aaf0a9643f36a96323e0099bdb352c2a62580b9
Benita <benita.isaac@gmail.com>
2024-04-11 00:00:40 -0700

    added responsiveness of about image

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -2,10 +2,12 @@ import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <div className='header-wrapper'>
+    <div className="header-wrapper">
       <div className="main">
-        <h1> Benita's portfolio </h1>
-        <Navigation />
+        <div className="header-content">
+          <h1> Benita's portfolio </h1>
+          <Navigation />
+        </div>
       </div>
     </div>
   );
